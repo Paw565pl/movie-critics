ec0762c6d85c253fe743313aa0cd2bfeaf16cf90
Paw565pl <paw565pl@gmail.com>
2024-08-23 12:59:13 +0200

    chore: configure swagger ui and openapi v3 paths

--- build.gradle.kts
+++ build.gradle.kts
@@ -34,7 +34,7 @@ repositories {
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-actuator")
-    implementation("org.springdoc:springdoc-openapi-starter-webmvc-api:2.6.0")
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.6.0")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.springframework.boot:spring-boot-starter-security")
