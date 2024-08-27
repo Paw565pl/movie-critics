39525dc734955c96cb461b79fd797089a6f3bf45
Paw565pl <paw565pl@gmail.com>
2024-08-27 14:37:59 +0200

    feat: create movie response classes

---
+++ src/main/java/dev/paw565pl/movieCritics/movie/response/MovieResponse.java
@@ -0,0 +1,32 @@
+package dev.paw565pl.movieCritics.movie.response;
+
+import java.time.LocalDate;
+import java.time.Year;
+import java.util.List;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class MovieResponse {
+
+    private Long id;
+    private String title;
+    private Year year;
+    private String rated;
+    private LocalDate released;
+    private String runtime;
+    private List<GenreResponse> genres;
+    private List<DirectorResponse> directors;
+    private List<WriterResponse> writers;
+    private List<ActorResponse> actors;
+    private String plot;
+    private String language;
+    private String country;
+    private String awards;
+    private String poster;
+    private String metaScore;
+    private String dvd;
+    private String boxOffice;
+    private String website;
+}
