df315baa2cdd24f84d9dfab21ca497f4e79cb17f
Paw565pl <paw565pl@gmail.com>
2024-08-28 14:01:30 +0200

    feat: create error response class

---
+++ src/main/java/dev/paw565pl/movie_critics/handler/ErrorResponse.java
@@ -0,0 +1,36 @@
+package dev.paw565pl.movie_critics.handler;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Map;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class ErrorResponse {
+
+    private LocalDateTime timestamp;
+    private Integer status;
+    private String error;
+    private String message;
+    private Map<String, List<String>> errors;
+
+    public ErrorResponse(
+            Integer status, String error, String message, Map<String, List<String>> errors) {
+        this.timestamp = LocalDateTime.now();
+        this.status = status;
+        this.error = error;
+        this.message = message;
+        this.errors = errors;
+    }
+
+    public ErrorResponse(Integer status, String error, String message) {
+        this.timestamp = LocalDateTime.now();
+        this.status = status;
+        this.error = error;
+        this.message = message;
+    }
+}
