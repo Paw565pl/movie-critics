cd4c77ebb8fc39c64e0d716f36232e81884b7727
Paw565pl <paw565pl@gmail.com>
2024-08-28 22:30:22 +0200

    fix: add missing JpaSpecificationExecutor to MovieRepository

--- src/main/java/dev/paw565pl/movie_critics/movie/repository/MovieRepository.java
+++ src/main/java/dev/paw565pl/movie_critics/movie/repository/MovieRepository.java
@@ -2,5 +2,7 @@ package dev.paw565pl.movie_critics.movie.repository;
 
 import dev.paw565pl.movie_critics.movie.model.Movie;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-public interface MovieRepository extends JpaRepository<Movie, Long> {}
+public interface MovieRepository
+        extends JpaRepository<Movie, Long>, JpaSpecificationExecutor<Movie> {}
