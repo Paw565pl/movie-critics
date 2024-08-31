4b7ebfc783311a6945cdcbf91268907ce10092c7
Paw565pl <paw565pl@gmail.com>
2024-08-31 15:15:44 +0200

    feat: create oauth2 resource server config

--- src/main/java/dev/paw565pl/movie_critics/config/SecurityConfig.java
+++ src/main/java/dev/paw565pl/movie_critics/config/SecurityConfig.java
@@ -1,24 +1,34 @@
 package dev.paw565pl.movie_critics.config;
 
+import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
+
+import dev.paw565pl.movie_critics.auth.KeycloakJwtConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests((auth) -> auth.anyRequest().permitAll());
 
+        http.oauth2ResourceServer(
+                (oauth2) ->
+                        oauth2.jwt(
+                                (jwt) ->
+                                        jwt.jwtAuthenticationConverter(
+                                                new KeycloakJwtConverter())));
+
         http.csrf(AbstractHttpConfigurer::disable);
-        http.sessionManagement(
-                (session) -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        http.sessionManagement((session) -> session.sessionCreationPolicy(STATELESS));
 
         return http.build();
     }
