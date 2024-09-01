d15abc38d91787ed2673ed03fb85f40015d87dba
Paw565pl <paw565pl@gmail.com>
2024-09-01 17:45:24 +0200

    feat: create rating repository

---
+++ src/main/java/dev/paw565pl/movie_critics/rating/repository/RatingRepository.java
@@ -0,0 +1,11 @@
+package dev.paw565pl.movie_critics.rating.repository;
+
+import dev.paw565pl.movie_critics.rating.model.Rating;
+import java.util.Optional;
+import java.util.UUID;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RatingRepository extends JpaRepository<Rating, Long> {
+
+    Optional<Rating> findByMovieIdAndUserId(Long movieId, UUID userId);
+}
