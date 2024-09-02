6dc61cae8d13e420903f34e618523966a9fc6cbf
Paw565pl <paw565pl@gmail.com>
2024-09-02 17:39:21 +0200

    refactor: use UserDetails implementation for getting user info

--- src/main/java/dev/paw565pl/movie_critics/rating/service/RatingService.java
+++ src/main/java/dev/paw565pl/movie_critics/rating/service/RatingService.java
@@ -1,19 +1,19 @@
 package dev.paw565pl.movie_critics.rating.service;
 
+import dev.paw565pl.movie_critics.auth.UserDetailsImpl;
 import dev.paw565pl.movie_critics.movie.exception.MovieNotFoundException;
-import dev.paw565pl.movie_critics.movie.model.Movie;
 import dev.paw565pl.movie_critics.movie.repository.MovieRepository;
 import dev.paw565pl.movie_critics.rating.dto.RatingDto;
 import dev.paw565pl.movie_critics.rating.model.Rating;
 import dev.paw565pl.movie_critics.rating.repository.RatingRepository;
 import dev.paw565pl.movie_critics.rating.response.RatingResponse;
+import java.util.UUID;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.server.ResponseStatusException;
-
-import java.util.UUID;
 
 @Service
 public class RatingService {
@@ -26,26 +26,25 @@ public class RatingService {
         this.movieRepository = movieRepository;
     }
 
-    private Movie findMovieById(Long id) {
-        return movieRepository.findById(id).orElseThrow(MovieNotFoundException::new);
-    }
-
     private Rating findRating(Long movieId, UUID userId) {
-        findMovieById(movieId);
         return ratingRepository
                 .findByMovieIdAndUserId(movieId, userId)
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
     }
 
-    public RatingResponse findByMovieIdAndUserId(Long movieId, UUID userId) {
-        var rating = findRating(movieId, userId);
+    public RatingResponse findByMovieIdAndUserId(Long movieId, Jwt jwt) {
+        var user = UserDetailsImpl.fromJwt(jwt);
+        var rating = findRating(movieId, user.getId());
+
         return new RatingResponse(rating.getValue());
     }
 
     @Transactional
-    public RatingResponse create(Long movieId, UUID userId, RatingDto dto) {
-        var movie = findMovieById(movieId);
-        var rating = new Rating(dto.value(), userId, movie);
+    public RatingResponse create(Long movieId, Jwt jwt, RatingDto dto) {
+        var movie = movieRepository.findById(movieId).orElseThrow(MovieNotFoundException::new);
+        var user = UserDetailsImpl.fromJwt(jwt);
+
+        var rating = new Rating(dto.value(), user.getUsername(), user.getId(), movie);
 
         try {
             var savedRating = ratingRepository.saveAndFlush(rating);
@@ -56,8 +55,10 @@ public class RatingService {
     }
 
     @Transactional
-    public RatingResponse update(Long movieId, UUID userId, RatingDto dto) {
-        var rating = findRating(movieId, userId);
+    public RatingResponse update(Long movieId, Jwt jwt, RatingDto dto) {
+        var user = UserDetailsImpl.fromJwt(jwt);
+        var rating = findRating(movieId, user.getId());
+
         rating.setValue(dto.value());
 
         var savedRating = ratingRepository.save(rating);
@@ -65,8 +66,10 @@ public class RatingService {
     }
 
     @Transactional
-    public void delete(Long movieId, UUID userId) {
-        var rating = findRating(movieId, userId);
+    public void delete(Long movieId, Jwt jwt) {
+        var user = UserDetailsImpl.fromJwt(jwt);
+        var rating = findRating(movieId, user.getId());
+
         ratingRepository.deleteById(rating.getId());
     }
 }
