c4b89b4cc30e8b771156f94a2f2872026624afab
Paw565pl <paw565pl@gmail.com>
2024-09-08 15:51:11 +0200

    feat: create methods to retrieve movies to watch by userId

--- src/main/java/dev/paw565pl/movie_critics/movie/repository/MovieRepository.java
+++ src/main/java/dev/paw565pl/movie_critics/movie/repository/MovieRepository.java
@@ -2,6 +2,9 @@ package dev.paw565pl.movie_critics.movie.repository;
 
 import dev.paw565pl.movie_critics.movie.model.Movie;
 import java.util.Optional;
+import java.util.UUID;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
@@ -15,4 +18,8 @@ public interface MovieRepository
 
     @Query("SELECT ROUND(AVG(r.value), 2) FROM Rating r WHERE r.movie.id = :movieId")
     Optional<Double> findAverageRatingByMovieId(@Param("movieId") Long movieId);
+
+    Page<Movie> findAllByUsersWhoWantToWatchId(UUID userId, Pageable pageable);
+
+    Optional<Movie> findByIdAndUsersWhoWantToWatchId(Long id, UUID userId);
 }
