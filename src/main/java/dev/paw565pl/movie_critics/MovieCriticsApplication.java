12c90b5cb4dcbe1486b7aad3f590cae4577cd547
Paw565pl <paw565pl@gmail.com>
2024-09-10 14:58:51 +0200

    fix: page serialization warning

--- src/main/java/dev/paw565pl/movie_critics/MovieCriticsApplication.java
+++ src/main/java/dev/paw565pl/movie_critics/MovieCriticsApplication.java
@@ -2,8 +2,11 @@ package dev.paw565pl.movie_critics;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
+import org.springframework.data.web.config.EnableSpringDataWebSupport.PageSerializationMode;
 
 @SpringBootApplication
+@EnableSpringDataWebSupport(pageSerializationMode = PageSerializationMode.VIA_DTO)
 public class MovieCriticsApplication {
 
     public static void main(String[] args) {
