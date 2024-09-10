1b93db30b0e7026e8652b1a226c586d5c9a2d724
Paw565pl <paw565pl@gmail.com>
2024-09-10 16:44:36 +0200

    feat: create query method for finding top active users

--- src/main/java/dev/paw565pl/movie_critics/user/repository/UserRepository.java
+++ src/main/java/dev/paw565pl/movie_critics/user/repository/UserRepository.java
@@ -2,6 +2,20 @@ package dev.paw565pl.movie_critics.user.repository;
 
 import dev.paw565pl.movie_critics.user.model.User;
 import java.util.UUID;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
-public interface UserRepository extends JpaRepository<User, UUID> {}
+public interface UserRepository extends JpaRepository<User, UUID> {
+    @Query(
+            """
+                SELECT u FROM User u
+                LEFT JOIN Rating r ON r.author = u
+                LEFT JOIN Comment c ON c.author = u
+                GROUP BY u.id
+                HAVING COUNT(r.id) + COUNT(c.id) > 0
+                ORDER BY COUNT(r.id) + COUNT(c.id) ASC
+            """)
+    Page<User> findAllOrderByRatingsAndCommentsCount(Pageable pageable);
+}
