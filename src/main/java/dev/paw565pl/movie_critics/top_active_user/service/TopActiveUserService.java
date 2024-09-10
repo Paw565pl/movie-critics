36aa81041c9524f0ac3088bd33b1d3ba9112ce30
Paw565pl <paw565pl@gmail.com>
2024-09-10 16:49:24 +0200

    feat: create TopActiveUserService

---
+++ src/main/java/dev/paw565pl/movie_critics/top_active_user/service/TopActiveUserService.java
@@ -0,0 +1,27 @@
+package dev.paw565pl.movie_critics.top_active_user.service;
+
+import dev.paw565pl.movie_critics.top_active_user.mapper.TopActiveUserMapper;
+import dev.paw565pl.movie_critics.top_active_user.response.TopActiveUserResponse;
+import dev.paw565pl.movie_critics.user.repository.UserRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TopActiveUserService {
+
+    private final UserRepository userRepository;
+    private final TopActiveUserMapper topActiveUserMapper;
+
+    public TopActiveUserService(
+            UserRepository userRepository, TopActiveUserMapper topActiveUserMapper) {
+        this.userRepository = userRepository;
+        this.topActiveUserMapper = topActiveUserMapper;
+    }
+
+    public Page<TopActiveUserResponse> findTop10ActiveUsers() {
+        return userRepository
+                .findAllOrderByRatingsAndCommentsCount(Pageable.ofSize(10))
+                .map(topActiveUserMapper::toResponse);
+    }
+}
