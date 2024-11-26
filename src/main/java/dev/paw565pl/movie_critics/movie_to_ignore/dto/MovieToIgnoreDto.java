25850aa3b8be2376a1ab02d405898628dabbba34
Paw565pl <paw565pl@gmail.com>
2024-11-26 20:04:54 +0100

    refactor: minor quality adjustments

--- src/main/java/dev/paw565pl/movie_critics/movie_to_ignore/dto/MovieToIgnoreDto.java
+++ src/main/java/dev/paw565pl/movie_critics/movie_to_ignore/dto/MovieToIgnoreDto.java
@@ -2,4 +2,5 @@ package dev.paw565pl.movie_critics.movie_to_ignore.dto;
 
 import jakarta.validation.constraints.NotNull;
 
-public record MovieToIgnoreDto(@NotNull(message = "MovieId cannot be null.") Long movieId) {}
+public record MovieToIgnoreDto(@NotNull(message = "Movie id cannot be null.") Long movieId) {
+}
--- src/main/java/dev/paw565pl/movie_critics/movie_to_ignore/service/MovieToIgnoreService.java
+++ src/main/java/dev/paw565pl/movie_critics/movie_to_ignore/service/MovieToIgnoreService.java
@@ -1,14 +1,13 @@
 package dev.paw565pl.movie_critics.movie_to_ignore.service;
 
 import dev.paw565pl.movie_critics.auth.details.UserDetailsImpl;
-import dev.paw565pl.movie_critics.movie.exception.MovieNotFoundException;
 import dev.paw565pl.movie_critics.movie.mapper.MovieMapper;
 import dev.paw565pl.movie_critics.movie.model.MovieEntity;
 import dev.paw565pl.movie_critics.movie.repository.MovieRepository;
 import dev.paw565pl.movie_critics.movie.response.MovieResponse;
+import dev.paw565pl.movie_critics.movie.service.MovieService;
 import dev.paw565pl.movie_critics.movie_to_ignore.dto.MovieToIgnoreDto;
 import dev.paw565pl.movie_critics.user.repository.UserRepository;
-import java.util.UUID;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -18,25 +17,27 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.util.UUID;
+
 @Service
 public class MovieToIgnoreService {
-    private final UserRepository userRepository;
+
     private final MovieRepository movieRepository;
     private final MovieMapper movieMapper;
+    private final MovieService movieService;
+    private final UserRepository userRepository;
 
-    public MovieToIgnoreService(
-            UserRepository userRepository,
-            MovieRepository movieRepository,
-            MovieMapper movieMapper) {
-        this.userRepository = userRepository;
+    public MovieToIgnoreService(MovieRepository movieRepository, MovieMapper movieMapper, MovieService movieService, UserRepository userRepository) {
         this.movieRepository = movieRepository;
         this.movieMapper = movieMapper;
+        this.movieService = movieService;
+        this.userRepository = userRepository;
     }
 
     private MovieEntity findMovieToIgnore(Long movieId, UUID userId) {
         return movieRepository
                 .findByIdAndUsersWhoIgnoredId(movieId, userId)
-                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Movie with given id is not in your ignored list."));
     }
 
     public Page<MovieResponse> findAll(Jwt jwt, Pageable pageable) {
@@ -49,19 +50,12 @@ public class MovieToIgnoreService {
     @Transactional
     public MovieResponse create(Jwt jwt, MovieToIgnoreDto dto) {
         var userId = UserDetailsImpl.fromJwt(jwt).getId();
-        var movie =
-                movieRepository
-                        .findById(dto.movieId())
-                        .orElseThrow(
-                                () ->
-                                        new ResponseStatusException(
-                                                HttpStatus.BAD_REQUEST,
-                                                new MovieNotFoundException().getMessage()));
-        var user = userRepository.findById(userId).orElseThrow();
+        var userEntity = userRepository.findById(userId).orElseThrow();
+        var movie = movieService.findEntity(dto.movieId());
 
         try {
-            user.getIgnoredMovies().add(movie);
-            userRepository.saveAndFlush(user);
+            userEntity.getIgnoredMovies().add(movie);
+            userRepository.saveAndFlush(userEntity);
         } catch (DataIntegrityViolationException e) {
             throw new DataIntegrityViolationException("Movie is already in your ignored list.");
         }
@@ -74,8 +68,9 @@ public class MovieToIgnoreService {
         var userId = UserDetailsImpl.fromJwt(jwt).getId();
         var movieToIgnore = findMovieToIgnore(movieId, userId);
 
-        var user = userRepository.findById(userId).orElseThrow();
-        user.getIgnoredMovies().remove(movieToIgnore);
-        userRepository.save(user);
+        var userEntity = userRepository.findById(userId).orElseThrow();
+        userEntity.getIgnoredMovies().remove(movieToIgnore);
+
+        userRepository.save(userEntity);
     }
 }
