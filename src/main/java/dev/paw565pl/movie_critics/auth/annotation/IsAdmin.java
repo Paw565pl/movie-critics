8385d62eb655676199ec4e19bb2b6bd8dde9728b
Paw565pl <paw565pl@gmail.com>
2024-11-26 19:25:50 +0100

    refactor: remove redundant method

--- src/main/java/dev/paw565pl/movie_critics/auth/annotation/IsAdmin.java
+++ src/main/java/dev/paw565pl/movie_critics/auth/annotation/IsAdmin.java
@@ -1,12 +1,14 @@
 package dev.paw565pl.movie_critics.auth.annotation;
 
+import org.springframework.security.access.prepost.PreAuthorize;
+
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
-import org.springframework.security.access.prepost.PreAuthorize;
 
 @Target({ElementType.METHOD, ElementType.TYPE})
 @Retention(RetentionPolicy.RUNTIME)
-@PreAuthorize("hasRole(T(dev.paw565pl.movie_critics.auth.role.Role).ADMIN.toString())")
-public @interface IsAdmin {}
+@PreAuthorize("hasRole(T(dev.paw565pl.movie_critics.auth.role.Role).ADMIN.name())")
+public @interface IsAdmin {
+}
--- src/main/java/dev/paw565pl/movie_critics/auth/role/Role.java
+++ src/main/java/dev/paw565pl/movie_critics/auth/role/Role.java
@@ -3,13 +3,10 @@ package dev.paw565pl.movie_critics.auth.role;
 public enum Role {
     ADMIN;
 
-    /** Returns the role name with the 'ROLE_' prefix. */
+    /**
+     * Returns the role name with the 'ROLE_' prefix.
+     */
     public String getPrefixedRole() {
         return "ROLE_" + name();
-    }
-
-    @Override
-    public String toString() {
-        return name();
     }
 }
