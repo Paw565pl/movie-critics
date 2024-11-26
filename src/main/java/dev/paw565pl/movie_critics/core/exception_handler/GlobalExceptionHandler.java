4a18c7d1a0857dceae6d36d54442f21d1c655795
Paw565pl <paw565pl@gmail.com>
2024-11-26 19:21:59 +0100

    feat: add handler for HttpMessageNotReadableException

--- src/main/java/dev/paw565pl/movie_critics/core/exception_handler/GlobalExceptionHandler.java
+++ src/main/java/dev/paw565pl/movie_critics/core/exception_handler/GlobalExceptionHandler.java
@@ -1,42 +1,46 @@
 package dev.paw565pl.movie_critics.core.exception_handler;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@ControllerAdvice
+import java.util.HashMap;
+import java.util.List;
+
+
+@RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<ErrorResponse> handleMethodArgumentNotValidException(
-            MethodArgumentNotValidException e) {
+    public ResponseEntity<ErrorResponse> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
         var errors = new HashMap<String, List<String>>();
-        e.getBindingResult()
-                .getFieldErrors()
-                .forEach(
-                        (f) -> {
-                            var fieldName = f.getField();
-                            var errorMessage = f.getDefaultMessage();
+        e.getBindingResult().getFieldErrors().forEach((f) -> {
+            var fieldName = f.getField();
+            var errorMessage = f.getDefaultMessage();
 
-                            if (errorMessage == null) return;
+            if (errorMessage == null) return;
 
-                            if (errors.containsKey(fieldName)) {
-                                errors.get(fieldName).add(errorMessage);
-                            } else {
-                                errors.put(fieldName, new ArrayList<>(List.of(errorMessage)));
-                            }
-                        });
+            if (errors.containsKey(fieldName)) {
+                errors.get(fieldName).add(errorMessage);
+            } else {
+                errors.put(fieldName, List.of(errorMessage));
+            }
+        });
 
         var status = HttpStatus.BAD_REQUEST;
-        var response =
-                new ErrorResponse(
-                        status.value(), status.getReasonPhrase(), "Validation failed.", errors);
+        var response = new ErrorResponse(status.value(), status.getReasonPhrase(), "Validation failed.", errors);
+
+        return ResponseEntity.status(status).body(response);
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<ErrorResponse> handleHttpMessageNotReadableException(HttpMessageNotReadableException e) {
+        var status = HttpStatus.BAD_REQUEST;
+        var response = new ErrorResponse(status.value(), status.getReasonPhrase(), "Required request body is missing.");
 
         return ResponseEntity.status(status).body(response);
     }
