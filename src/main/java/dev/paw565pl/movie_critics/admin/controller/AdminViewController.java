27e58b47d65e29f04187730c75f238c1647458ba
Paw565pl <paw565pl@gmail.com>
2024-11-30 14:07:42 +0100

    feat: implement adding movies for admin

---
+++ src/main/java/dev/paw565pl/movie_critics/admin/controller/AdminViewController.java
@@ -0,0 +1,110 @@
+package dev.paw565pl.movie_critics.admin.controller;
+
+import dev.paw565pl.movie_critics.auth.annotation.IsAdmin;
+import dev.paw565pl.movie_critics.movie.dto.MovieDto;
+import dev.paw565pl.movie_critics.movie.dto.MovieFormDto;
+import dev.paw565pl.movie_critics.movie.repository.ActorRepository;
+import dev.paw565pl.movie_critics.movie.repository.DirectorRepository;
+import dev.paw565pl.movie_critics.movie.repository.GenreRepository;
+import dev.paw565pl.movie_critics.movie.repository.WriterRepository;
+import dev.paw565pl.movie_critics.movie.service.MovieService;
+import jakarta.validation.Valid;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/admin")
+public class AdminViewController {
+
+    private final MovieService movieService;
+    private final GenreRepository genreRepository;
+    private final DirectorRepository directorRepository;
+    private final WriterRepository writerRepository;
+    private final ActorRepository actorRepository;
+
+    public AdminViewController(
+            MovieService movieService,
+            GenreRepository genreRepository,
+            DirectorRepository directorRepository,
+            WriterRepository writerRepository,
+            ActorRepository actorRepository) {
+        this.movieService = movieService;
+        this.genreRepository = genreRepository;
+        this.directorRepository = directorRepository;
+        this.writerRepository = writerRepository;
+        this.actorRepository = actorRepository;
+    }
+
+    private void populateMovieForm(Model model) {
+        var ageRatingOptions = movieService.findDistinctAgeRatings();
+        var genreOptions = genreRepository.findAll();
+        var directorOptions = directorRepository.findAll();
+        var writerOptions = writerRepository.findAll();
+        var actorOptions = actorRepository.findAll();
+
+        model.addAttribute("ageRatingOptions", ageRatingOptions);
+        model.addAttribute("genreOptions", genreOptions);
+        model.addAttribute("directorOptions", directorOptions);
+        model.addAttribute("writerOptions", writerOptions);
+        model.addAttribute("actorOptions", actorOptions);
+    }
+
+    @IsAdmin
+    @GetMapping
+    public String getAdminPanelView(Model model) {
+        populateMovieForm(model);
+        model.addAttribute(
+                "movieFormData",
+                new MovieFormDto(
+                        null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null,
+                        null));
+
+        return "admin/admin";
+    }
+
+    @IsAdmin
+    @PostMapping("/movies/save")
+    public String saveMovie(
+            @Valid @ModelAttribute("movieFormData") MovieFormDto movieFormDto,
+            BindingResult bindingResult,
+            Model model) {
+        if (bindingResult.hasErrors()) {
+            populateMovieForm(model);
+            return "admin/admin";
+        }
+
+        try {
+            var movieDto = new MovieDto(
+                    movieFormDto.title(),
+                    movieFormDto.ageRating(),
+                    movieFormDto.released(),
+                    movieFormDto.runtime(),
+                    movieFormDto.plot(),
+                    movieFormDto.language(),
+                    movieFormDto.country(),
+                    movieFormDto.awards(),
+                    movieFormDto.poster(),
+                    movieFormDto.metaScore(),
+                    movieFormDto.dvd(),
+                    movieFormDto.boxOffice(),
+                    movieFormDto.website(),
+                    movieFormDto.genreIds(),
+                    movieFormDto.directorIds(),
+                    movieFormDto.writerIds(),
+                    movieFormDto.actorIds());
+            movieService.create(movieDto);
+        } catch (DataIntegrityViolationException e) {
+            bindingResult.rejectValue("title", "", e.getMessage());
+            populateMovieForm(model);
+            return "admin/admin";
+        }
+
+        return "redirect:/admin";
+    }
+}
---
+++ src/main/java/dev/paw565pl/movie_critics/movie/dto/MovieFormDto.java
@@ -0,0 +1,28 @@
+package dev.paw565pl.movie_critics.movie.dto;
+
+import jakarta.validation.constraints.*;
+import java.time.LocalDate;
+import java.util.List;
+
+public record MovieFormDto(
+        @NotBlank(message = "Title cannot be empty.")
+                @Size(min = 2, max = 200, message = "Title must be between 2 and 200 characters.")
+                String title,
+        @Size(max = 255, message = "Age must be at most 255 characters.") String ageRating,
+        @NotNull(message = "Released date cannot be null.") LocalDate released,
+        @Size(max = 255, message = "Runtime must be at most 255 characters.") String runtime,
+        @Size(min = 10, max = 1000, message = "Plot must be between 10 and 1000 characters.") String plot,
+        @Size(max = 255, message = "Language must be at most 255 characters.") String language,
+        @Size(max = 255, message = "Country must be at most 255 characters.") String country,
+        @Size(max = 255, message = "Awards must be at most 255 characters.") String awards,
+        @Size(max = 255, message = "Poster must be at most 255 characters.") String poster,
+        @Max(value = 100, message = "Metascore must be less than or equal to 100.")
+                @Min(value = 0, message = "Metascore must be greater than or equal to 0.")
+                Short metaScore,
+        @Size(max = 255, message = "Dvd must be at most 255 characters.") String dvd,
+        @Size(max = 255, message = "Box office must be at most 255 characters.") String boxOffice,
+        @Size(max = 255, message = "Website must be at most 255 characters.") String website,
+        @NotEmpty(message = "Genre ids cannot be empty.") List<Long> genreIds,
+        List<Long> directorIds,
+        List<Long> writerIds,
+        List<Long> actorIds) {}
