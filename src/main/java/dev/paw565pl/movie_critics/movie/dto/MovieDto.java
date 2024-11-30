49b11ed4c1eb16be775bf65f148ac47092d6d06b
Paw565pl <paw565pl@gmail.com>
2024-11-30 14:08:08 +0100

    feat: add better validation to movie dto

--- src/main/java/dev/paw565pl/movie_critics/movie/dto/MovieDto.java
+++ src/main/java/dev/paw565pl/movie_critics/movie/dto/MovieDto.java
@@ -1,9 +1,6 @@
 package dev.paw565pl.movie_critics.movie.dto;
 
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 import java.time.LocalDate;
 import java.util.List;
 
@@ -11,18 +8,20 @@ public record MovieDto(
         @NotBlank(message = "Title cannot be empty.")
                 @Size(min = 2, max = 200, message = "Title must be between 2 and 200 characters.")
                 String title,
-        String ageRating,
+        @Size(min = 2, max = 255, message = "Age rating must be between 2 and 255 characters.") String ageRating,
         @NotNull(message = "Released date cannot be null.") LocalDate released,
-        String runtime,
+        @Size(min = 2, max = 255, message = "Runtime must be between 2 and 255 characters.") String runtime,
         @Size(min = 10, max = 1000, message = "Plot must be between 10 and 1000 characters.") String plot,
-        String language,
-        String country,
-        String awards,
-        String poster,
-        Short metaScore,
-        String dvd,
-        String boxOffice,
-        String website,
+        @Size(min = 2, max = 255, message = "Language must be between 2 and 255 characters.") String language,
+        @Size(min = 2, max = 255, message = "Country must be between 2 and 255 characters.") String country,
+        @Size(min = 2, max = 255, message = "Awards must be between 2 and 255 characters.") String awards,
+        @Size(min = 2, max = 255, message = "Poster must be between 2 and 255 characters.") String poster,
+        @Max(value = 100, message = "Metascore must be less than or equal to 100.")
+                @Min(value = 0, message = "Metascore must be greater than or equal to 0.")
+                Short metaScore,
+        @Size(min = 2, max = 255, message = "Dvd must be between 2 and 255 characters.") String dvd,
+        @Size(min = 2, max = 255, message = "Box office must be between 2 and 255 characters.") String boxOffice,
+        @Size(min = 2, max = 255, message = "Website must be between 2 and 255 characters.") String website,
         @NotEmpty(message = "Genre ids cannot be empty.") List<Long> genreIds,
         List<Long> directorIds,
         List<Long> writerIds,
