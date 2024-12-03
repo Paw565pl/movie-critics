15b69079954f0a2d8b01c8a6913dec65da5a415b
Paw565pl <paw565pl@gmail.com>
2024-12-03 18:49:15 +0100

    test: create IntegrationTest base class

---
+++ src/test/java/dev/paw565pl/movie_critics/IntegrationTest.java
@@ -0,0 +1,31 @@
+package dev.paw565pl.movie_critics;
+
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+
+import io.restassured.RestAssured;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ActiveProfiles;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@Testcontainers
+@DirtiesContext
+@ActiveProfiles("test")
+@SpringBootTest(webEnvironment = RANDOM_PORT)
+public abstract class IntegrationTest {
+
+    @LocalServerPort
+    private Integer port;
+
+    @Container
+    @ServiceConnection
+    private static final PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:17-alpine");
+
+    protected void setBaseUrl(String baseUrl) {
+        RestAssured.baseURI = "http://localhost:" + port + baseUrl;
+    }
+}
