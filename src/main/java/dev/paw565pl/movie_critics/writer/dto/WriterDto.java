67a4f5236caaf2393511b519532c6d21b099d91a
Paw565pl <paw565pl@gmail.com>
2024-12-04 11:11:54 +0100

    chore: add custom error message

--- src/main/java/dev/paw565pl/movie_critics/writer/dto/WriterDto.java
+++ src/main/java/dev/paw565pl/movie_critics/writer/dto/WriterDto.java
@@ -4,4 +4,6 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
 public record WriterDto(
-        @NotBlank @Size(min = 2, max = 255, message = "Name must be between 2 and 255 characters long.") String name) {}
+        @NotBlank(message = "Name cannot be empty.")
+                @Size(min = 2, max = 255, message = "Name must be between 2 and 255 characters long.")
+                String name) {}
